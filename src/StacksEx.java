a6c5b8c3b3e9d4cde46aafd6f63af53db8c1c96a
Jayden <jayden.jimenez005@gmail.com>
2023-02-15 21:50:50 -0800

    Initial commit

---
+++ src/StacksEx.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class StacksEx
+{
+    public static void main(String[] args)
+    {
+        Stack<String> videoGames = new Stack<String>();
+
+        videoGames.add("Super Mario Brothers WII");
+        videoGames.add("Sons Of the Forest");
+        videoGames.add("Smite Battleground of The Gods");
+        videoGames.add("Call of Duty Modern Warfare 2");
+        videoGames.add("Dead By Daylight");
+
+        System.out.println(videoGames);
+    }
+}
